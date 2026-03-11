36707a33b742512d7936906af774980e4962fb7c
alexdev-sudo <ezequielcastm123@gmail.com>
2026-03-10 19:45:32 -0600

    moviento de test a stage

---
+++ Expresiones_2Listener.py
@@ -0,0 +1,147 @@
+# Generated from Expresiones_2.g4 by ANTLR 4.13.1
+from antlr4 import *
+if "." in __name__:
+    from .Expresiones_2Parser import Expresiones_2Parser
+else:
+    from Expresiones_2Parser import Expresiones_2Parser
+
+# This class defines a complete listener for a parse tree produced by Expresiones_2Parser.
+class Expresiones_2Listener(ParseTreeListener):
+
+    # Enter a parse tree produced by Expresiones_2Parser#root.
+    def enterRoot(self, ctx:Expresiones_2Parser.RootContext):
+        pass
+
+    # Exit a parse tree produced by Expresiones_2Parser#root.
+    def exitRoot(self, ctx:Expresiones_2Parser.RootContext):
+        pass
+
+
+    # Enter a parse tree produced by Expresiones_2Parser#bloque.
+    def enterBloque(self, ctx:Expresiones_2Parser.BloqueContext):
+        pass
+
+    # Exit a parse tree produced by Expresiones_2Parser#bloque.
+    def exitBloque(self, ctx:Expresiones_2Parser.BloqueContext):
+        pass
+
+
+    # Enter a parse tree produced by Expresiones_2Parser#statement.
+    def enterStatement(self, ctx:Expresiones_2Parser.StatementContext):
+        pass
+
+    # Exit a parse tree produced by Expresiones_2Parser#statement.
+    def exitStatement(self, ctx:Expresiones_2Parser.StatementContext):
+        pass
+
+
+    # Enter a parse tree produced by Expresiones_2Parser#varint.
+    def enterVarint(self, ctx:Expresiones_2Parser.VarintContext):
+        pass
+
+    # Exit a parse tree produced by Expresiones_2Parser#varint.
+    def exitVarint(self, ctx:Expresiones_2Parser.VarintContext):
+        pass
+
+
+    # Enter a parse tree produced by Expresiones_2Parser#asignacion.
+    def enterAsignacion(self, ctx:Expresiones_2Parser.AsignacionContext):
+        pass
+
+    # Exit a parse tree produced by Expresiones_2Parser#asignacion.
+    def exitAsignacion(self, ctx:Expresiones_2Parser.AsignacionContext):
+        pass
+
+
+    # Enter a parse tree produced by Expresiones_2Parser#ifstm.
+    def enterIfstm(self, ctx:Expresiones_2Parser.IfstmContext):
+        pass
+
+    # Exit a parse tree produced by Expresiones_2Parser#ifstm.
+    def exitIfstm(self, ctx:Expresiones_2Parser.IfstmContext):
+        pass
+
+
+    # Enter a parse tree produced by Expresiones_2Parser#expr.
+    def enterExpr(self, ctx:Expresiones_2Parser.ExprContext):
+        pass
+
+    # Exit a parse tree produced by Expresiones_2Parser#expr.
+    def exitExpr(self, ctx:Expresiones_2Parser.ExprContext):
+        pass
+
+
+    # Enter a parse tree produced by Expresiones_2Parser#logicalOr.
+    def enterLogicalOr(self, ctx:Expresiones_2Parser.LogicalOrContext):
+        pass
+
+    # Exit a parse tree produced by Expresiones_2Parser#logicalOr.
+    def exitLogicalOr(self, ctx:Expresiones_2Parser.LogicalOrContext):
+        pass
+
+
+    # Enter a parse tree produced by Expresiones_2Parser#logicalAnd.
+    def enterLogicalAnd(self, ctx:Expresiones_2Parser.LogicalAndContext):
+        pass
+
+    # Exit a parse tree produced by Expresiones_2Parser#logicalAnd.
+    def exitLogicalAnd(self, ctx:Expresiones_2Parser.LogicalAndContext):
+        pass
+
+
+    # Enter a parse tree produced by Expresiones_2Parser#igualdad.
+    def enterIgualdad(self, ctx:Expresiones_2Parser.IgualdadContext):
+        pass
+
+    # Exit a parse tree produced by Expresiones_2Parser#igualdad.
+    def exitIgualdad(self, ctx:Expresiones_2Parser.IgualdadContext):
+        pass
+
+
+    # Enter a parse tree produced by Expresiones_2Parser#comparacion.
+    def enterComparacion(self, ctx:Expresiones_2Parser.ComparacionContext):
+        pass
+
+    # Exit a parse tree produced by Expresiones_2Parser#comparacion.
+    def exitComparacion(self, ctx:Expresiones_2Parser.ComparacionContext):
+        pass
+
+
+    # Enter a parse tree produced by Expresiones_2Parser#suma.
+    def enterSuma(self, ctx:Expresiones_2Parser.SumaContext):
+        pass
+
+    # Exit a parse tree produced by Expresiones_2Parser#suma.
+    def exitSuma(self, ctx:Expresiones_2Parser.SumaContext):
+        pass
+
+
+    # Enter a parse tree produced by Expresiones_2Parser#producto.
+    def enterProducto(self, ctx:Expresiones_2Parser.ProductoContext):
+        pass
+
+    # Exit a parse tree produced by Expresiones_2Parser#producto.
+    def exitProducto(self, ctx:Expresiones_2Parser.ProductoContext):
+        pass
+
+
+    # Enter a parse tree produced by Expresiones_2Parser#unario.
+    def enterUnario(self, ctx:Expresiones_2Parser.UnarioContext):
+        pass
+
+    # Exit a parse tree produced by Expresiones_2Parser#unario.
+    def exitUnario(self, ctx:Expresiones_2Parser.UnarioContext):
+        pass
+
+
+    # Enter a parse tree produced by Expresiones_2Parser#primario.
+    def enterPrimario(self, ctx:Expresiones_2Parser.PrimarioContext):
+        pass
+
+    # Exit a parse tree produced by Expresiones_2Parser#primario.
+    def exitPrimario(self, ctx:Expresiones_2Parser.PrimarioContext):
+        pass
+
+
+
+del Expresiones_2Parser
