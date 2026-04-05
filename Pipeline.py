9d168034cfad86106e425adb2a99019dfeff4660
alexdev-sudo <ezequielcastm123@gmail.com>
2026-04-04 20:23:03 -0600

    funcionalidad(Pipeline):
    Implementamos flujo completo con fases lexica, sintactica, semantica y ejecucion

--- Pipeline.py
+++ Pipeline.py
@@ -12,38 +12,61 @@ def main():
 
     input_file = sys.argv[1] if len(sys.argv) > 1 else "entradas Expr.txt"
     input_stream = FileStream(input_file)
+    print("\n ----- Analisis lexico---------")
 
-    #Lexer 
+    #Lexer--------------------------------- 
+
     lexer = Expresiones21Lexer(input_stream)
-
-    #parser 
-    token_stream = CommonTokenStream(lexer)
-    parser = Expresiones21Parser(token_stream)
-
+    # Error lexico
     error_listener = CustomErrorListener()
+    lexer.removeErrorListeners()
+    lexer.addErrorListener(error_listener)  
+
+    
+    token_stream = CommonTokenStream(lexer)
+    token_stream.fill()
+
+    if error_listener.errors:
+        for e in error_listener.errors:
+            print(e)
+        print("\n Error en Fase Lexica, Pipeline detendo ")    
+        return
+    print("Lexico correcto, iniciando fase sintactica")
+
+    #Parser-----------------------------------
+    parser = Expresiones21Parser(token_stream)
+    # error_listener = CustomErrorListener()
     parser.removeErrorListeners()
     parser.addErrorListener(error_listener)
     
     tree = parser.root()
+
     #errores sintacticos
     if error_listener.errors:
         for e in error_listener.errors:
             print(e)
+        print("\n Error en Fase Sintactica, Pipeline detendo ")    
         return
-    
-    #semantico
+    print("Sintaxis correcta, iniciando fase semantica")
+
+    #semantico--------------------------------
+
     sematic = semanticVisitor()
     sematic.visit(tree)
 
     if sematic.errors:
         for e in sematic.errors:
             print(e)
+        print("\n Error en Fase Semantica, Pipeline detenido ")    
         return
-    
-    #Interpretar
+    print("Semantica correcta, iniciando fase de ejecucion")
+
+    #Interpretar--------------------------------
+
     interpreter = EvalVisitor()
     print("\n=== iniciando interpretación ===")
     interpreter.visit(tree)
+    
     print("\n=== interpretación finalizada ===")
 
 if __name__ == "__main__":
